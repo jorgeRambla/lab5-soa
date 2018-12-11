ded40d0c6a7d9596859415a6d48980d1c0232503
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2018-12-11 16:39:48 +0100

    Added dark-light mode switch

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
     registerTemplate();
 });
 
+function toggleDarkLight() {
+    var body = document.getElementById("body");
+    var currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  }
+
 function registerSearch() {
     $("#search").submit(function (ev) {
         event.preventDefault();
