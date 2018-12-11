89eef2772c0c4f8235c616f6d08e52abf81fd39f
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2018-12-11 11:47:08 +0100

    Added sorting function

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -9,6 +9,39 @@ function registerSearch() {
         $.get($(this).attr('action'), {q: $("#q").val(), 
                                     count: $("#qcount").val(),
                                     lang: $("#qlang :selected").val()}, function (data) {
+            var opt_selected = $("#qsort :selected").val();
+            switch(opt_selected) {
+                case "f_asc":
+                    data = data.sort(
+                        function(a, b) {
+                            return parseFloat(a.user.followersCount) - parseFloat(b.user.followersCount)
+                        }
+                    )
+                    break;
+                case "f_desc":
+                    data = data.sort(
+                        function(a, b) {
+                            return parseFloat(b.user.followersCount) - parseFloat(a.user.followersCount)
+                        }
+                    )
+                    break;
+                case "n_asc":
+                    data = data.sort(
+                        function(a, b) {
+                            return a.user.screenName.localeCompare(b.user.screenName);
+                        }
+                    )
+                    break;
+                case "n_desc":
+                    data = data.sort(
+                        function(a, b) {
+                            return b.user.screenName.localeCompare(a.user.screenName);
+                        }
+                    )
+                    break;  
+                default:
+                    break;
+            }
             $("#resultsBlock").html(Mustache.render(template, data));
         });
     });
